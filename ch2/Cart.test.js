cf988bea4a511490606b827fd8f0bd3b5fca1962
Russ Hyde <russ.hyde.data@gmail.com>
2021-12-31 20:58:51 +0000

    ch2: long-hand unit test for addToCart()

--- ch2/Cart.test.js
+++ ch2/Cart.test.js
@@ -1,1 +1,18 @@
 const Cart = require("./Cart.js");
+
+const cart = new Cart();
+cart.addToCart("cheesecake");
+
+const hasOneItem = cart.items.length === 1;
+const hasACheesecake = cart.items[0] === "cheesecake";
+
+if (hasOneItem && hasACheesecake) {
+  console.log("Adding an item works correctly");
+} else {
+  const actualContent = cart.items.join(", ");
+
+  console.error("addToCart failed!");
+  console.error(`Actual content of cart: ${actualContent}`);
+
+  throw new Error("Test failed!");
+}
