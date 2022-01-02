0fa50943f3b699eab1b5836c90e195e132da47c5
Russ Hyde <russ.hyde.data@gmail.com>
2022-01-02 20:10:05 +0000

    tja-ch2: use node's 'assert' module instead of self-rolled comparisons

--- test-js-apps/README.md
+++ test-js-apps/README.md
@@ -1,4 +1,9 @@
 # testing-js
+
 Notes on "Testing Javascript Applications"
 
 See [https://github.com/lucasfcosta/testing-javascript-applications]()
+
+## node.js tools
+
+- `assert` module: `assert.deepStrictEqual(object, expected)`
--- test-js-apps/ch2/Cart.test.js
+++ test-js-apps/ch2/Cart.test.js
@@ -1,18 +1,9 @@
+const assert = require("assert");
 const Cart = require("./Cart.js");
 
 const cart = new Cart();
 cart.addToCart("cheesecake");
 
-const hasOneItem = cart.items.length === 1;
-const hasACheesecake = cart.items[0] === "cheesecake";
+assert.deepStrictEqual(cart.items, ["cheesecake"]);
 
-if (hasOneItem && hasACheesecake) {
-  console.log("Adding an item works correctly");
-} else {
-  const actualContent = cart.items.join(", ");
-
-  console.error("addToCart failed!");
-  console.error(`Actual content of cart: ${actualContent}`);
-
-  throw new Error("Test failed!");
-}
+console.log("The addToCart function can add an item to the cart");
