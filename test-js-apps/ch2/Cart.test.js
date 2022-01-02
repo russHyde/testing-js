ab9470cfbb2cd4d0071c5531a3324b860204b79c
Russ Hyde <russ.hyde.data@gmail.com>
2022-01-02 20:19:41 +0000

    tja-ch2: add test for removeFromCart (dependent on the addToCart test, for now)

--- test-js-apps/ch2/Cart.test.js
+++ test-js-apps/ch2/Cart.test.js
@@ -2,8 +2,11 @@ const assert = require("assert");
 const Cart = require("./Cart.js");
 
 const cart = new Cart();
+
 cart.addToCart("cheesecake");
-
 assert.deepStrictEqual(cart.items, ["cheesecake"]);
-
 console.log("The addToCart function can add an item to the cart");
+
+cart.removeFromCart("cheesecake");
+assert.deepStrictEqual(cart.items, []);
+console.log("The removeFromCart function can remove an item from the cart");
