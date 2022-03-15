6e898b929305c51accdcd55e057c0ec21d9651df
Russ Hyde <russ.hyde.data@gmail.com>
2022-03-15 13:55:04 +0000

    refac: truncate db before each test case runs

--- test-js-apps/ch2/cart.test.js
+++ test-js-apps/ch2/cart.test.js
@@ -1,19 +1,20 @@
 const { db, closeConnection } = require("./dbConnection");
 const { createCart, addItem } = require("./cart");
 
+beforeEach(async () => {
+  await db("carts").truncate();
+  await db("carts_items").truncate();
+});
+
 afterAll(async () => await closeConnection());
 
 test("createCart creates a cart for a username", async () => {
-  await db("carts").truncate();
   await createCart("Russ Hyde");
   const result = await db.select("username").from("carts");
   expect(result).toEqual([{ username: "Russ Hyde" }]);
 });
 
 test("addItem adds an item to a cart", async () => {
-  await db("carts_items").truncate();
-  await db("carts").truncate();
-
   const username = "Lucas da Costa";
   await createCart(username);
   const { id: cartId } = await db.select().from("carts").where({ username });
