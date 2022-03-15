912fd3962a5898cbf5fac5b9775194fbfe90dfdd
Russ Hyde <russ.hyde.data@gmail.com>
2022-03-15 13:51:28 +0000

    [fail] test: can add an item to a cart (fails to acquire db connection)

--- test-js-apps/ch2/cart.test.js
+++ test-js-apps/ch2/cart.test.js
@@ -1,11 +1,27 @@
 const { db, closeConnection } = require("./dbConnection");
-const { createCart } = require("./cart");
+const { createCart, addItem } = require("./cart");
 
 test("createCart creates a cart for a username", async () => {
   await db("carts").truncate();
   await createCart("Russ Hyde");
   const result = await db.select("username").from("carts");
   expect(result).toEqual([{ username: "Russ Hyde" }]);
+
+  await closeConnection();
+});
+
+test("addItem adds an item to a cart", async () => {
+  await db("carts_items").truncate();
+  await db("carts").truncate();
+
+  const username = "Lucas da Costa";
+  await createCart(username);
+  const { id: cartId } = await db.select().from("carts").where({ username });
+
+  await addItem(cartId, "cheesecake");
+  const result = await db.select("itemName").from("carts_items");
+
+  expect(result).toEqual([{ cartId, itemName: "cheesecake" }]);
 
   await closeConnection();
 });
